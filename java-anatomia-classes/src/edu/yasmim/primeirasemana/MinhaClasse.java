f71684da570ba3313d63469b1c2fbca06d568233
Yasmim Santos <85970653+yasmimsantos@users.noreply.github.com>
2023-01-21 22:38:39 -0300

    Add files via upload

---
+++ java-anatomia-classes/src/edu/yasmim/primeirasemana/MinhaClasse.java
@@ -0,0 +1,19 @@
+package edu.yasmim.primeirasemana;
+
+public class MinhaClasse {
+
+  public static void main(String[] args) {
+    String primeiroNome = "Yasmim";
+    String segundoNome = "Santos";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+  }
+
+  public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+
+  }
+
+}
